3c0918c2e7619526496e85168aa3d7813800ff79
Karl-Einar Bergstrøm <45044399+karleinarb@users.noreply.github.com>
2022-02-09 11:08:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import { useSession } from '@vtfk/react-msal';
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const { isAuthenticated, login, authStatus, user } = useSession()
+  const [isMobile, setisMobile] = useState(window.innerWidth < 1000 ? true : false);
+
+  function handleWindowSizeChange() {
+      if(window.innerWidth < 1000) setisMobile(true);
+      else setisMobile(false);
+  }
+  useEffect(() => {
+      window.addEventListener('resize', handleWindowSizeChange);
+      return () => {
+        window.removeEventListener('resize', handleWindowSizeChange);
+      }
+  }, []);
+
+  function handleLogin() {
+    if(isMobile) login({ scopes: ['openid', 'profile', 'User.Read'], forceRefresh: true})
+    else login({ scopes: ['openid', 'profile', 'User.Read'], forceRefresh: true}, 'loginPopup')
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <div>Environment: {process.env.NODE_ENV}</div>
+        <div>User-agent: {window.navigator.userAgent}</div>
+        <div>Is mobile: {isMobile.toString()}</div>
         <div>Is authenticated?: {isAuthenticated.toString()}</div>
         <div>Authentication status: {authStatus}</div>
         {
-          !isAuthenticated && <button onClick={() => login({ scopes: ['openid', 'profile', 'User.Read'], forceRefresh: true}, 'loginPopup' )}>Login</button>
+          !isAuthenticated && <button onClick={() => handleLogin()}>Login</button>
         }
         {
           isAuthenticated && JSON.stringify(user)
