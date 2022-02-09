ccba9eb419c027c7c22eef64f209373a68e7a1fd
Karl-Einar Bergstrøm <45044399+karleinarb@users.noreply.github.com>
2022-02-09 11:55:47 +0100

    Påbegynt teams auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useSession } from '@vtfk/react-msal';
+import * as microsoftTeams from "@microsoft/teams-js";
 import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -6,21 +7,42 @@ import './App.css';
 function App() {
   const { isAuthenticated, login, authStatus, user } = useSession()
   const [isMobile, setisMobile] = useState(window.innerWidth < 1000 ? true : false);
+  const [authResponse, setAuthResponse] = useState('');
+  const [firstMounted, setFirstMounted] = useState(false);
 
   function handleWindowSizeChange() {
-      if(window.innerWidth < 1000) setisMobile(true);
-      else setisMobile(false);
+    if(window.innerWidth < 1000) setisMobile(true);
+    else setisMobile(false);
   }
+
   useEffect(() => {
-      window.addEventListener('resize', handleWindowSizeChange);
-      return () => {
-        window.removeEventListener('resize', handleWindowSizeChange);
-      }
+    microsoftTeams.initialize(() => {console.log('Library is ready!')})
+
+    window.addEventListener('resize', handleWindowSizeChange);
+    document.onload = () => { console.log('I am ready')}
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    }
   }, []);
 
+  function teamsSucessfullLoginHandler(response) {
+    console.log('Response:', response);
+    setAuthResponse(response);
+  }
+
   function handleLogin() {
-    if(isMobile) login({ scopes: ['openid', 'profile', 'User.Read'], forceRefresh: true})
-    else login({ scopes: ['openid', 'profile', 'User.Read'], forceRefresh: true}, 'loginPopup')
+    if(window.navigator.userAgent.toLowerCase().includes('teams')) {
+      console.log('Authenticating using teams')
+      microsoftTeams.authentication.authenticate({
+        url: window.location.origin,
+        width: 600,
+        height: 535,
+        successCallback: teamsSucessfullLoginHandler
+      })
+    } else {
+      if(isMobile) login({ scopes: ['openid', 'profile', 'User.Read'], forceRefresh: true})
+      else login({ scopes: ['openid', 'profile', 'User.Read'], forceRefresh: true}, 'loginPopup')
+    }
   }
 
   return (
@@ -31,6 +53,7 @@ function App() {
         <div>Is mobile: {isMobile.toString()}</div>
         <div>Is authenticated?: {isAuthenticated.toString()}</div>
         <div>Authentication status: {authStatus}</div>
+        <div>Authentication response:<br/>{authResponse}</div>
         {
           !isAuthenticated && <button onClick={() => handleLogin()}>Login</button>
         }
