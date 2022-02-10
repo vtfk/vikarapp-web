9f56fea637d51e4c66089521cf75c3fd7aa60b2d
Karl-Einar Bergstrøm <45044399+karleinarb@users.noreply.github.com>
2022-02-10 14:02:11 +0100

    Update Logout.jsx

--- src/routes/Logout.jsx
+++ src/routes/Logout.jsx
@@ -1,10 +1,11 @@
 import './Login.css'
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import * as auth from '../auth'
 
 export default function Logout() {
   const [isLoggingOut, setIsLoggingOut] = useState(true)
-
+  const navigate = useNavigate();
 
   useEffect(() => {
     setIsLoggingOut(true);
@@ -15,6 +16,9 @@ export default function Logout() {
   return(
     <div className='container'>
       <div className='title'>{isLoggingOut ? 'Logger ut' : 'Du har logget ut'}</div>
+      {
+        !isLoggingOut && <button onClick={() => { navigate('/login')}} style={{marginTop: '2rem'}}>Logg inn</button>
+      }
     </div>
   )
 }
