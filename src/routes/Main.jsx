f07fca249c60e689a449c0b48de0b7b51ac1e75a
Karl-Einar Bergstrøm <45044399+karleinarb@users.noreply.github.com>
2022-02-11 13:24:17 +0100

    Litt mer arbeid på layout

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,17 +1,24 @@
 import './Main.css'
+import logo from '../images/vtfk-logo.svg'
+
+import { Button } from '@vtfk/components'
 
 export default function Main() {
   return (
     <main className='main-container'>
       <div className='app-container'>
         <div className='main-header'>
-          <h1 style={{margin: 0, fontSize: '3rem'}}>VikarApp</h1>
+          <h1 className='main-title' style={{margin: 0, fontSize: '3rem'}}>VikarApp</h1>
         </div>
         <div className='main-content'>
-          Her skal det være noe
+          Her skal det være innhold
         </div>
         <div className='main-footer'>
-          Her skal det være en footer
+          <img src={logo} width="100px" />
+          <div className='main-footer-button-group'>
+            <Button size="small">Jeg skal være vikar</Button>
+            <Button size="small">Forleng vikariat</Button>
+          </div>
         </div>
       </div>
     </main>
