16146d6f79d95006612dde7f9eea8cb808b592f9
Karl-Einar Bergstrøm <45044399+karleinarb@users.noreply.github.com>
2022-02-11 17:22:02 +0100

    Arbeid med table

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,4 +1,6 @@
 import './style.css'
+import { Checkbox } from '@vtfk/components'
+import { useState } from 'react';
 
 /*
   Props
@@ -11,22 +13,63 @@ function random() {
   return Math.floor(Math.random() * 10000000000)
 }
 
-export default function Table({items, headers, id = 'id', style}) {
+export default function Table({items, headers, itemId = '_id', style, dense = false, showSelect = true, selectOnClick = true}) {
+  // State
+  const [selectedIds, setSelectedIds] = useState([])
+  const [selectedItems, setSelectedItems] = useState([])
+  const [isAllSelected, setIsAllSelected] = useState(false)
+
+  // Functions
+  function updateSelected(item, isSelected) {
+    if(isSelected) {
+      if(!selectedIds.includes(item[itemId])) {
+        setSelectedIds([...selectedIds, item[itemId]]);
+        setSelectedItems([...selectedItems, item]);
+      }
+    } else {
+      setSelectedIds(selectedIds.filter((i) => i !== item[itemId]))
+      setSelectedItems(selectedItems.filter((i) => i[itemId] !== item[itemId]))
+    }
+    if(items.length > 0 && selectedIds.length > 0 && selectedIds.length === items.length) setIsAllSelected(true);
+    else setIsAllSelected(false);
+  }
+
+  function selectAll(bool) {
+    if(bool) {
+      setSelectedIds(items.map((i) => i[itemId]))
+      setSelectedItems(items);
+      setIsAllSelected(true);
+    } else {
+      setSelectedIds([])
+      setSelectedItems([]);
+      setIsAllSelected(false);
+    }
+  }
+
+  function isSelected(item) {
+    return selectedIds.includes(item[itemId]);
+  }
+
+  // Render function
   return(
     <table className="vtfk-table" style={style} cellSpacing="0" cellPadding="0">
       <thead>
-        <tr>{ headers.map((header) => <th key={random()} className={header.class} style={header.style}>{header.label}</th>) }</tr>
+        <tr>
+          { showSelect && <th><Checkbox checked={isAllSelected} name={"checkAll"} value={"checkAll"} label={" "} onChange={(e) => selectAll(e.target.checked)} style={{padding: 0}}/></th>}
+          { headers.map((header) => <th key={random()} className={header.class} style={header.style}>{header.label}</th>) }
+        </tr>
       </thead>
       <tbody>
         {
           (items && Array.isArray(items) && items.length > 0) ?
-          items.map((item, i) => {
+          items.map((item) => {
             return (
-              <tr key={item[id]}>
+              <tr key={item[itemId]} onClick={() => selectOnClick && updateSelected(item, true)} className={isSelected(item) && 'tr-selected'}>
+                { showSelect && <td><Checkbox checked={isSelected(item)} onChange={(e) => updateSelected(item, e.target.checked)} /></td>}
                 {
                   headers.map((header) => {
                     return (
-                      <td key={random()} style={header.itemStyle}>{item[header.value] || ''}</td>
+                      <td key={random()} className={dense && 'td-dense'} style={header.itemStyle}>{item[header.value] || ''}</td>
                     )
                   })
                 }
--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -27,18 +27,18 @@ export default function Main() {
 
   const items = [
     {
-      id: 'd95fac91-38d3-4a9a-b473-cc3af4143e61',
+      _id: 'd95fac91-38d3-4a9a-b473-cc3af4143e61',
       status: 'active',
       team: 'Section_2021-Test av vikarapp',
       teacher: 'per.test@vtfk.no',
       expiration: '13.02.2022'
     },
     {
-      id: '30e43a62-4d43-4e1f-8e5a-8c8055819ffe',
+      _id: '30e43a62-4d43-4e1f-8e5a-8c8055819ffe',
       status: 'active',
       team: 'Section_2021-Test av vikarapp',
       teacher: 'per.test@vtfk.no',
-      expiration: '13.02.2022'
+      expiration: '14.02.2022'
     }
   ]
 
@@ -49,7 +49,7 @@ export default function Main() {
           <h1 className='main-title' style={{margin: 0, fontSize: '3rem'}}>VikarApp</h1>
         </div>
         <div className='main-content'>
-          <Table headers={headers} items={items} />
+          <Table itemId="_id" headers={headers} items={items} />
         </div>
         <div className='main-footer'>
           <img className='main-footer-logo' alt="footer-logo" src={logo} width="100px" />
