b0b6947092dbd66e4f454fa4d9f72d49b03bbcaf
Karl-Einar Bergstrøm <45044399+karleinarb@users.noreply.github.com>
2022-02-14 11:28:47 +0100

    Forbedret layout litt

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,6 +1,5 @@
 import './Main.css'
 import logo from '../images/vtfk-logo.svg'
-import { useState } from 'react'
 import { IconButton} from '@vtfk/components'
 import {
   Routes,
@@ -12,53 +11,10 @@ import {
   Import nested routes
 */
 import Substitute from './main/substitute';
-
-import { Button } from '@vtfk/components'
-import Table from '../components/Table'
-
+import Overview from './main/overview/'
 
 
 export default function Main() {
-  const headers = [
-    {
-      label: 'Status',
-      value: 'status',
-      itemStyle: { textTransform: 'capitalize' }
-    },
-    {
-      label: 'Team',
-      value: 'team'
-    },
-    {
-      label: 'Lærer',
-      value: 'teacher'
-    },
-    {
-      label: 'Utløper',
-      value: 'expiration'
-    }
-  ]
-
-  const items = [
-    {
-      _id: 'd95fac91-38d3-4a9a-b473-cc3af4143e61',
-      status: 'active',
-      team: 'Section_2021-Test av vikarapp',
-      teacher: 'per.test@vtfk.no',
-      expiration: '13.02.2022'
-    },
-    {
-      _id: '30e43a62-4d43-4e1f-8e5a-8c8055819ffe',
-      status: 'active',
-      team: 'Section_2021-Test av vikarapp',
-      teacher: 'per.test@vtfk.no',
-      expiration: '14.02.2022'
-    }
-  ]
-
-  const [selectedIds, setSelectedIds] = useState([])
-  const [selectedItems, setSelectedItems] = useState([])
-
   return (
     <main className='main-container'>
       <div className='app-container'>
@@ -73,19 +29,11 @@ export default function Main() {
         <div className='main-content'>
           <Routes>
             <Route path="substitute" element={<Substitute />} />
-            <Route path="/" element={<Table itemId="_id" headers={headers} items={items} selected={selectedIds} onSelectedIdsChanged={(e) => setSelectedIds(e)} onSelectedItemsChanged={(e) => setSelectedItems(e)} />} />
+            <Route path="/" element={<Overview />} />
           </Routes>
-          { selectedIds }
         </div>
         <div className='main-footer'>
           <img className='main-footer-logo' alt="footer-logo" src={logo} width="100px" />
-          <div className='main-footer-button-group'>
-            <Link to="substitute">
-              <Button size="small">Jeg skal være vikar</Button>
-            </Link>
-            
-            <Button size="small" disabled={selectedItems.length === 0}>Forleng vikariat</Button>
-          </div>
         </div>
       </div>
     </main>
---
+++ src/routes/main/overview/index.jsx
@@ -0,0 +1,60 @@
+import './style.css'
+import Table from '../../../components/Table'
+import { Button } from '@vtfk/components'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function MainOverview() {
+  // State
+  const [selectedIds, setSelectedIds] = useState([])
+  const [selectedItems, setSelectedItems] = useState([])
+
+  const headers = [
+    {
+      label: 'Status',
+      value: 'status',
+      itemStyle: { textTransform: 'capitalize' }
+    },
+    {
+      label: 'Team',
+      value: 'team'
+    },
+    {
+      label: 'Lærer',
+      value: 'teacher'
+    },
+    {
+      label: 'Utløper',
+      value: 'expiration'
+    }
+  ]
+
+  const items = [
+    {
+      _id: 'd95fac91-38d3-4a9a-b473-cc3af4143e61',
+      status: 'active',
+      team: 'Section_2021-Test av vikarapp',
+      teacher: 'per.test@vtfk.no',
+      expiration: '13.02.2022'
+    },
+    {
+      _id: '30e43a62-4d43-4e1f-8e5a-8c8055819ffe',
+      status: 'active',
+      team: 'Section_2021-Test av vikarapp',
+      teacher: 'per.test@vtfk.no',
+      expiration: '14.02.2022'
+    }
+  ]
+
+  return (
+    <div className="overview">
+      <Table itemId="_id" headers={headers} items={items} selected={selectedIds} onSelectedIdsChanged={(e) => setSelectedIds(e)} onSelectedItemsChanged={(e) => setSelectedItems(e)} />
+      <div className='main-footer-button-group'>
+        <Link to="substitute">
+          <Button size="small">Jeg skal være vikar</Button>
+        </Link>
+        <Button size="small" disabled={selectedItems.length === 0}>Forleng vikariat</Button>
+      </div>
+    </div>
+  )
+}
