a975d8045a8dfaef508762fc1d4c4d7b98ef870a
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2022-02-24 02:00:11 +0100

    Temporary fix for deployment

--- src/routes/admin/substituterelationships/index.jsx
+++ src/routes/admin/substituterelationships/index.jsx
@@ -90,6 +90,7 @@ export default function SubstituteRelationships() {
   useEffect(() => {
     function getData () { get() }
     getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   function onEditItem(item) {
