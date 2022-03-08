f8c5ac03db898c985256d4e75a0c08e2d4b11291
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2022-03-08 16:35:49 +0100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,16 @@
 import './style.css'
 import { IconButton } from "@vtfk/components";
 import { useNavigate, useLocation } from "react-router-dom"
-import { getValidToken } from '../../auth'
+import { hasRole } from '../../auth'
 
 export default function Header() {
   const location = useLocation();
   const navigate = useNavigate();
   
   let isAdmin = false;
-  const token = getValidToken();
-  if(['development', 'test'].includes(process.env.NODE_ENV))
-  if(!isAdmin && token?.roles && Array.isArray(token.roles)) {
-    isAdmin = token.roles.some((i) => ['App.Admin', 'App.Config'].includes(i))
-  }
+
+  if(['development', 'test'].includes(process.env.NODE_ENV)) isAdmin = true;
+  isAdmin = isAdmin || hasRole(['App.Admin', 'App.Config'])
 
   return (
     <div className="header">
