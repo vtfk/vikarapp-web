81e712095fedfb30a1b77f696011212faab7cbd6
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2022-04-07 14:27:41 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   VTFK_VIKARAPI_BASEURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7070/api/' : process.env.REACT_APP_VTFK_VIKARAPI_BASEURL,
-  VTFK_VIKARAPI_APPKEY: process.env.REACT_APP_AZF_APPKEY,
+  VTFK_VIKARAPI_APPKEY: process.env.REACT_APP_VTFK_VIKARAPI_APPKEY,
   USE_MOCK: process.env.USE_MOCK || false
 }
 
